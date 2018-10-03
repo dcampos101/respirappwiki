f2000a34fbb57a699a212dbe2771d5652e67b124
granbahamut <ivan.dario.pinillagmail.com>
2018-10-02 20:19:12 -0500

    Modifiación de la pagina para corregir los links, se quitaron los estilos apra poder ajustar la maqueta de la pagina principal

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,17 @@
 Rails.application.routes.draw do
   get 'pages/datamodels'
-
   get 'pages/ermodels'
-
   get 'pages/complaints'
-
   get 'pages/maps'
-
   get 'pages/contacts'
-
   get 'welcome/index'
   root :to =>"welcome#index"
+
+  resources :complaints
+  resources :contacts
+  resources :datamodels
+  resources :ermodels
+  resources :maps
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
